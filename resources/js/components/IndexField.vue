b1f1946b603da7528fe8819d5a4d7779b462cb8a
kennedymutisya <kenmsh@gmail.com>
2022-06-04 13:11:49 +0300

    fix: remove console

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -13,7 +13,6 @@ export default {
     }
   },
   created() {
-    console.log(this.field);
     this.image = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${this.field.attribute}`
   },
 }
