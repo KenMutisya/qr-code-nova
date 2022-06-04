3a7a35ac442a274edba407486380d635373470ba
kennedymutisya <kenmsh@gmail.com>
2022-06-04 14:03:13 +0300

    fix: Ensure detail field follows display convetion

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -22,7 +22,11 @@ export default {
     }
   },
   created() {
-    this.image = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${this.field.attribute}`
+    if (this.field.value !== null) {
+      this.image = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${this.field.value}`;
+    } else {
+      this.image = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${this.field.attribute}`;
+    }
   },
 }
 </script>
