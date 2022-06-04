d73e548c86594022fba83614b46fdd3dc5796df6
kennedymutisya <kenmsh@gmail.com>
2022-06-04 14:02:02 +0300

    fix: update js

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,5 +1,16 @@
 <template>
-  <img :src="image" alt="">
+  <div
+      class="flex flex-col md:flex-row -mx-6 px-6 py-2 md:py-0 space-y-2 md:space-y-0 border-t border-gray-100 dark:border-gray-700">
+    <div class="md:w-1/4 md:py-3">
+      <h4 class="font-bold md:font-normal">
+        <span>QR Code</span>
+      </h4>
+    </div>
+    <div class="md:w-3/4 md:py-3 break-all lg:break-words">
+      <img :src="image" alt="">
+    </div>
+  </div>
+
 </template>
 
 <script>
